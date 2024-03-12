d3d62240a51b9f82f18f48255bd3a37044a6f3bd
Meow Roar <sareacct91@gmail.com>
2024-03-12 13:40:37 -0700

    Setup User Model

---
+++ model/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    match: /^([a-z0-9\._-]+)@([a-z0-9\.-]+)\.([a-z]{2,6})$/,
+  },
+  thoughts: {
+    type: [{
+      type: Schema.Types.ObjectId,
+      ref: 'thought',
+    }],
+  },
+  friends: {
+    type: [{
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+    }],
+  },
+}, {
+  toJSON: {
+    virtuals: true,
+  },
+  id: false,
+});
+
+userSchema.virtual('friendCount')
+  .get(function() {
+    return this.friends.length;
+  })
+
+const User = model('user', userSchema);
+
+
+module.exports = User;
