0fab79eb9a5165a96946fc6d60a845e68602421d
Meow Roar <sareacct91@gmail.com>
2024-03-13 16:12:27 -0700

    setup routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const c = require('../../controller/userControllers');
+
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const seed = require('../utils/seed/seed');
 
 router.use('/api', apiRoutes);
+router.patch('/seed', seed)
 
 module.exports = router;
