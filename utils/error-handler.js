130317711be69189373dfcac5d9893fef60bb6a0
Meow Roar <sareacct91@gmail.com>
2024-03-14 14:26:03 -0700

    added custom error to error handler

--- utils/error-handler.js
+++ utils/error-handler.js
@@ -1,7 +1,12 @@
-function errorHandler (err, req, res, next) {
+function errorHandler(err, req, res, next) {
   console.error(err);
 
-  res.status(500).json({ err });
+  const customError = {
+    msg: err.message || 'Something went wrong. Try again later.',
+    code: err.code || 500
+  }
+
+  res.status(customError.code).json({ msg: customError.msg });
 };
 
 module.exports = errorHandler;
