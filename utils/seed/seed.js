a15c5d88570316ce07451e388979447c7bc048ef
Meow Roar <sareacct91@gmail.com>
2024-03-14 13:15:51 -0700

    update types

--- utils/seed/seed.js
+++ utils/seed/seed.js
@@ -1,15 +1,39 @@
 const connection = require('../../config/connection');
 const { Thought, User } = require('../../model');
 const { getRandomUser, getReaction, randomPick, getThought } = require('./data');
-const {ObjectId} = require('mongoose').Types
+const { ObjectId } = require('mongoose').Types
+const type = require('../typedef');
+
 
 async function seed(req, res) {
   // drop all collections in db
   (await connection.db.listCollections().toArray()).map((e) => e.name).forEach((e) => connection.dropCollection(e));
 
-  /**
-    @type {Array<import('../typedef').user>}
-   */
+
+  const users = getUsersObject();
+  const thoughts = getThoughtObject(users);
+
+  const thoughtsData = await Thought.create(thoughts);
+  const usersData = await User.create(users);
+
+  // seed user thoughts array
+  thoughtsData.forEach(async (t) => {
+    const i = usersData.findIndex((u) => u.username === t.username);
+    await usersData[i].updateOne({ $push: { thoughts: new ObjectId(t._id) } });
+  })
+
+  // seed user friends array
+  usersData.forEach(async (u, i) => {
+    i = i === usersData.length - 1 ? 0 : i + 1;
+    await u.updateOne({ $push: { friends: new ObjectId(usersData[i]._id) } });
+  })
+
+  res.status(201).json({ msg: 'seeded', thoughtsData, usersData });
+}
+
+
+function getUsersObject() {
+  /**@type {type.user[]}*/
   const users = new Array(50);
 
   for (let i = 0; i < users.length; i++) {
@@ -19,39 +43,35 @@ async function seed(req, res) {
     users[i] = { username, email };
   }
 
+  return users;
+}
 
+/**
+ * @param {type.user[]} users
+ */
+function getThoughtObject(users) {
+  /**@type {type.thought[]}*/
   const thoughts = new Array(100);
 
   for (let i = 0; i < thoughts.length; i++) {
     const thoughtText = getThought();
-    const username = /**@type {import('../typedef').user}*/(randomPick(users)).username;
+    const username = /**@type {type.user}*/(randomPick(users)).username;
+
+    /**@type {type.reaction[]}*/
     const reactions = new Array(20);
 
     for (let i = 0; i < reactions.length; i++) {
       const reactionBody = getReaction();
-      const username = /**@type {import('../typedef').user}*/(randomPick(users)).username;
+      const username = /**@type {type.user}*/(randomPick(users)).username;
 
       reactions[i] = { reactionBody, username };
     }
 
     thoughts[i] = { thoughtText, username, reactions };
   }
-
-  const thoughtsData = await Thought.create(thoughts);
-  const usersData = await User.create(users);
-
-
-  thoughtsData.forEach(async (t) => {
-    const i = usersData.findIndex((u) => u.username === t.username);
-    await usersData[i].updateOne({ $push: { thoughts: new ObjectId(t._id) } });
-  })
-
-  usersData.forEach(async (u, i) => {
-    i = i === usersData.length - 1 ? 0 : i + 1;
-    await u.updateOne({ $push: { friends: new ObjectId(usersData[i]._id) } });
-  })
-
-  res.status(201).json({ msg: 'seeded', thoughtsData, usersData });
+  return thoughts;
 }
+
+
 
 module.exports = seed;
--- utils/typedef.js
+++ utils/typedef.js
@@ -1,8 +1,20 @@
-  /**
-   * @typedef {Object} user
-   * @property {string} user.username
-   * @property {string} user.email
-  */
+/**
+ * @typedef {Object} user
+ * @property {string} user.username
+ * @property {string} user.email
+*/
 
+/**
+ * @typedef {Object} thought
+ * @property {string} thought.thoughtText
+ * @property {string} thought.username
+ * @property {Array<reaction>} thought.reactions
+ */
+
+/**
+ * @typedef {Object} reaction
+ * @property {string} reaction.reactionBody
+ * @property {string} reaction.username
+ */
 
 module.exports.unused = {};
