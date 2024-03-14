d59022d18cca302809bec82f8957d1ef46c5ef85
Meow Roar <sareacct91@gmail.com>
2024-03-14 14:25:43 -0700

    Finish setup user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 const c = require('../../controller/userControllers');
 
+router.route('/')
+  .get(c.findUsers)
+  .post(c.createUser)
 
+router.route('/:id')
+  .get(c.findOneUser)
+  .put(c.updateUser)
+  .delete(c.deleteUser)
 
 module.exports = router;
