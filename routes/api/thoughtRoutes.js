908506052bbd03b5a5591d389a8b5e8383ba18e3
Meow Roar <sareacct91@gmail.com>
2024-03-15 18:26:34 -0700

    added thoughtRoutes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,5 +1,18 @@
 const router = require('express').Router();
+const c = require('../../controller/thoughtControllers');
 
 
+router.route('/')
+  .get(c.findThoughts)
+  .post(c.createThought)
+
+router.route('/:thoughtId')
+  .get(c.findOneThought)
+  .put(c.updateThought)
+  .delete(c.deleteThought)
+
+router.route('/:thoughtId/reactions')
+  .post(c.addReaction)
+  .delete(c.deleteReaction)
 
 module.exports = router;
