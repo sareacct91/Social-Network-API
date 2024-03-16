2d7539ed29c3f9d830666c59f635e2832907c793
Meow Roar <sareacct91@gmail.com>
2024-03-16 01:28:42 -0700

    added functionality to update reaction's username when user's username is change

--- model/User.js
+++ model/User.js
@@ -1,5 +1,4 @@
-const { Schema, model, default: mongoose } = require('mongoose');
-const Thought = require('./Thought');
+const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
   username: {
@@ -39,14 +38,19 @@ userSchema.virtual('friendCount')
   })
 
 userSchema.post('findOneAndUpdate', async function (doc) {
-  if (doc) {
-    await Thought.updateMany({ _id: { $in: doc.thoughts } }, { username: doc.username });
+  if (doc && doc.thoughts.length) {
+    const oldUsername = (await model('thought').findById(doc.thoughts[0])).username;
+    const q1 = model('thought').updateMany({ _id: { $in: doc.thoughts } }, { username: doc.username }).exec();
+    const q2 = model('thought').updateMany({ 'reactions.username': oldUsername }, { $set: { 'reactions.$[element].username': doc.username } }, {arrayFilters: [{'element.username': oldUsername}]}).exec();
+
+    const [res1, res2] = await Promise.all([q1, q2]);
+    console.log(res1, res2);
   }
 })
 
 userSchema.post('findOneAndDelete', async function (doc) {
   if (doc) {
-    await Thought.deleteMany({ _id: { $in: doc.thoughts } });
+    await model('thought').deleteMany({ _id: { $in: doc.thoughts } });
   }
 })
 
